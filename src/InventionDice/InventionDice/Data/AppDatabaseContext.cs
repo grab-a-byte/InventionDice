f197ffd6596ba8bcc718ba116f1ae7f4ffff28d8
parkeradam <30392071+parkeradam@users.noreply.github.com>
2019-11-02 22:18:32 +0000

    Pushing missed dependencies/files

---
+++ src/InventionDice/InventionDice/Data/AppDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using InventionDice.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventionDice.Data
+{
+    internal class AppDatabaseContext : DbContext
+    {
+        private readonly IFileSystemPathHelper pathHelper;
+
+        public AppDatabaseContext(IFileSystemPathHelper pathHelper)
+        {
+            this.pathHelper = pathHelper;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={this.pathHelper.GetAppDataFilePath(ApplicationConstants.DatabaseName)}");
+    }
+}
